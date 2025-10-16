945e07a95938f9ce05c412c4098a99b6af01eaff
jagadeeshg0801 <jagadeeshg0801@gmail.com>
2025-10-16 11:15:14 +0530

    Login OTP , organizationPhonenumber added for API

--- src/app/login/password-change/password-change.component.ts
+++ src/app/login/password-change/password-change.component.ts
@@ -147,7 +147,8 @@ export class PasswordChangeComponent implements OnInit {
           this.authService.validateEmailOTP({
               "email":  localStorage.getItem('loggedUser'),
               "tempEmail": sessionStorage.getItem('tempEMail') ? sessionStorage.getItem('tempEMail') : '',
-              "emailOtp": this.otpNumber.toString()
+              "emailOtp": this.otpNumber.toString(),
+              "organizationPhonenumber": ('+91' + localStorage.getItem('loggedUserMobile')).toString(),
             }
             ).subscribe((res: any) => {
               if (res && res.status.toLowerCase() == 'success') {
