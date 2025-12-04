046b009a01f2c8ed1205fd5480913e047895152a
jagadeeshg0801 <jagadeeshg0801@gmail.com>
2025-12-04 21:35:06 +0530

    buyer info modal changes

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -37,6 +37,7 @@ export const SystemViewConfig = {
 }
 
 export const GMT_SYSTEM_SCREENS_LIST ={
+    'CategoryManager' :['/categorymgr/create-rfq', '/categorymgr/gmt-rqfs', '/categorymgr/client-gmt-rqfs', '/categorymgr/vendor-summary'], // 3 Screens
     'CategoryManager2':['/categorymgr/gmt-rqfs', '/categorymgr/gmt-summary','/categorymgr/gmt-reg-clients','/categorymgr/client-gmt-rqfs', '/categorymgr/gmt-vendors'], // 4 Screens
    'ClientInitiator':['/categorymgr/create-rfq', '/categorymgr/gmt-rfq-support','/categorymgr/gmt-rfq-faqs', '/categorymgr/my-profile'], // 3 Screens
 //    'Registration': [ '/categorymgr/vendor-profile',  '/categorymgr/gmt-rfq-faqs', '/categorymgr/my-profile' ], // 2 Screens
