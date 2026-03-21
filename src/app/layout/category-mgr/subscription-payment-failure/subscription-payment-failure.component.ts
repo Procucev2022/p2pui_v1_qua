b78a04ee1be564a048acc8534316131eac9e6e02
jagadeeshg0801 <jagadeeshg0801@gmail.com>
2026-03-21 16:37:36 +0530

    css changes

--- src/app/layout/category-mgr/subscription-payment-failure/subscription-payment-failure.component.ts
+++ src/app/layout/category-mgr/subscription-payment-failure/subscription-payment-failure.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-subscription-payment-failure',
   templateUrl: './subscription-payment-failure.component.html',
   styleUrls: ['./subscription-payment-failure.component.scss']
 })
-export class SubscriptionPaymentFailureComponent {
+export class SubscriptionPaymentFailureComponent implements OnInit  {
 
+
+    timeLeft: number = 25;
+    interval: any;
+    constructor(private router: Router) { }
+  
+    ngOnInit(): void {
+      this.startTimer();
+    }
+  
+    startTimer() {
+      this.interval = setInterval(() => {
+        this.timeLeft--;
+        if (this.timeLeft <= 0) {
+          clearInterval(this.interval);
+        }
+        if (this.timeLeft == 0) {
+          this.router.navigate(['/categorymgr/vendor-profile-subscriptions']);
+        }
+      }, 1000);
+    }
 }
--- src/app/layout/category-mgr/subscription-payment-success/subscription-payment-success.component.ts
+++ src/app/layout/category-mgr/subscription-payment-success/subscription-payment-success.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-subscription-payment-success',
   templateUrl: './subscription-payment-success.component.html',
   styleUrls: ['./subscription-payment-success.component.scss']
 })
-export class SubscriptionPaymentSuccessComponent {
+export class SubscriptionPaymentSuccessComponent implements OnInit  {
+ 
 
+  timeLeft: number = 25;
+  interval: any;
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+    this.startTimer();
+  }
+
+  startTimer() {
+    this.interval = setInterval(() => {
+      this.timeLeft--;
+      if (this.timeLeft <= 0) {
+        clearInterval(this.interval);
+      }
+      if (this.timeLeft == 0) {
+        this.router.navigate(['/categorymgr/vendor-profile-subscriptions']);
+      }
+    }, 1000);
+  }
 }
