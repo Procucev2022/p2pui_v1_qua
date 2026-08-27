2a3c4f2ff7431a7911b6621065f8b9b746fa62e6
navinprocucev-09 <navin.procucev@gmail.com>
2026-08-27 11:31:04 +0530

    Update pincode-control.component.spec.ts

--- src/app/shared/modules/common-share/components/pincode-control/pincode-control.component.spec.ts
+++ src/app/shared/modules/common-share/components/pincode-control/pincode-control.component.spec.ts
@@ -111,12 +111,10 @@ describe('PincodeControlComponent', () => {
     component.updatePincodeValidationStatus.subscribe(spy);
     component.onChangePinCode();
     expect(component.isValid).toBe(false);
-   expect(spy).toHaveBeenCalledWith(
-      expect(spy).toHaveBeenCalledWith({
-  pincodeIsValid: false,
-  pincode: '',
-});
-   );
+    expect(spy).toHaveBeenCalledWith({
+    pincodeIsValid: false,
+    pincode: '',
+    });
   });
 
   it('should validate standalone inputValue success and failure', () => {
