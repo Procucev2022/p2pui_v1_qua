22b9a3c5ed3fcf0065de4999ff7e1ad7adbddbeb
navinprocucev-09 <navin.procucev@gmail.com>
2026-08-31 16:31:04 +0530

    fix: remove dangling buyer-vendors module reference not present on this branch

--- src/app/layout/category-mgr/category-mgr-routing.module.ts
+++ src/app/layout/category-mgr/category-mgr-routing.module.ts
@@ -69,7 +69,6 @@ const routes: Routes = [
     {path: 'payment-success', component: SubscriptionPaymentSuccessComponent},
     {path: 'payment-failure', component: SubscriptionPaymentFailureComponent},
     {path: 'cat-mgr-reports', component: CategoryMgrReportsComponent},
-    {path: 'buyer-vendors', loadChildren: () => import('../buyer-vendors/buyer-vendors.module').then(m => m.BuyerVendorsModule)},
     {path: 'analytics', loadChildren: () => import('./analytics/analytics.module').then(m => m.AnalyticsModule)}
 ];
 @NgModule({
